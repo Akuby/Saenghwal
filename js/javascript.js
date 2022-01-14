eb4f3faae56f654da55f3e413dd44b59ddb56c1e
Hyewon Ryu <akuby1997@gmail.com>
2022-01-14 21:22:07 +0900

    야간모드 js 객체로 묶기

--- js/javascript.js
+++ js/javascript.js
@@ -1,45 +1,72 @@
-function linkColor(color,h_color,s_color) {
-  var alist = document.querySelectorAll('a');
-  for(var i = 1; i < alist.length; i++)
-    alist[i].style.color = color;
-  alist[0].style.color = h_color;
-  if (document.getElementById('active')){
-    document.querySelector('#active').style.color = s_color;
+var Title = {
+  setColor : function (color) {
+    document.querySelector('a').style.color = color;
   }
 }
 
-function bodyColor(color,b_color) {
-  var target = document.querySelector('body');
-  target.style.color = color;
-  target.style.backgroundColor = b_color;
+var Menu = {
+  setColor : function (color,a_color) {
+  var alist = document.querySelectorAll('a');
+  for(var i = 1; i < alist.length; i++){
+    alist[i].style.color = color; }
+  if (document.getElementById('active')){
+    document.querySelector('#active').style.color = a_color;
+    }
+  }
 }
 
-function buttonColor(color,s_color) {
-  var target = document.querySelector('input');
-  target.style.color = s_color;
-  target.style.backgroundColor = color;
+var Body = {
+  setTitleColor : function (color) {
+    document.querySelector('h2').style.color = color;
+  },
+  
+  setColor : function (color) {
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor : function (color) {
+    document.querySelector('body').style.backgroundColor = color;
+  }
 }
 
-function borderColor(color) {
-  document.querySelector('section').style.border = color;
+var Button = {
+  setColor : function (color) {
+    document.querySelector('input').style.color = color;
+  },
+  setBackgroundColor : function (color) {
+    document.querySelector('input').style.backgroundColor = color;
+  }
+}
+
+var Border = {
+  setColor : function (color) {
+    document.querySelector('section').style.border = color;
   document.querySelector('ol').style.borderRight = color;
   document.querySelector('ol').style.borderBottom = color;
   document.querySelector('h1').style.borderBottom = color;
+  }
 }
 
 function day_night_change(self) {
-  if (self.value == 'night') {
-    bodyColor('white','#313336');
-    buttonColor('white','black');
-    linkColor('#a8e1ff','#feffa8','#e07e46');
-    borderColor('2px solid white');
+  if (self.value == 'night') { //주간
+    Title.setColor('black'); 
+    Menu.setColor('black','black');
+    Body.setTitleColor('black');
+    Body.setColor('black');
+    Body.setBackgroundColor('white');
+    Button.setColor('black'); 
+    Button.setBackgroundColor('white'); 
+    Border.setColor('2px solid black'); 
     self.value = 'day';
   }
-  else {
-    bodyColor('black','white');
-    buttonColor('black','white');
-    linkColor('black','black','black');
-    borderColor('2px solid black');
+  else { //야간
+    Title.setColor('rgb(217, 133, 80)');
+    Menu.setColor('white','#ffffab');
+    Body.setTitleColor('#abe3ff');
+    Body.setColor('white');
+    Body.setBackgroundColor('black');
+    Button.setColor('white');
+    Button.setBackgroundColor('black');
+    Border.setColor('2px solid white');
     self.value = 'night';
   }
 }
